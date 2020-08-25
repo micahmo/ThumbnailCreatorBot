e4d41f29ba234098b718a0a213b63d5ccb11d278
Micah Morrison <micahmorrison95@gmail.com>
2020-08-25 08:14:34 -0400

    Update docker release script to support remote host

--- ThumbnailCreatorBot/rebuild_docker_release.ps1
+++ ThumbnailCreatorBot/rebuild_docker_release.ps1
@@ -1,3 +1,7 @@
+# Set the Docker host to the first command-line argument (if any)
+# Example usage: .\rebuild_docker_release.ps1 ssh://user@server
+$env:docker_host = $args[0]
+
 # Rebuild/publish the code
 dotnet publish -c Release
 
@@ -8,5 +12,9 @@ docker rmi thumbnailcreatorbot-release
 
 # Rebuild the image, recreate the container, and start the container
 docker build -t thumbnailcreatorbot-release -f Dockerfile_release .
-docker create --restart always --name thumbnailcreatorbot-release-container thumbnailcreatorbot-release
-docker start thumbnailcreatorbot-release-container
+
+# Use Docker run to create + start the container
+# -d: Detached mode (so that we're not stuck with stdout in tty)
+docker run -d --restart always --name thumbnailcreatorbot-release-container thumbnailcreatorbot-release
+
+$env:docker_host = ""
